a0f5d20f80331886b36079ccde504c4a82498ac4
Rohit <rohit.ku25112001@gmail.com>
2023-12-09 15:09:57 +0530

    admin panel

--- components/admin/add-route-subheading/add-route/index.jsx
+++ components/admin/add-route-subheading/add-route/index.jsx
@@ -4,38 +4,33 @@ import {
     Input,
     Button,
     message,
+    Empty,
+    Row,
+    Col,
+    Card,
     Menu,
     Dropdown,
     Image,
-    TimePicker,
-    Select
+    Select,
+    TimePicker
  } from "antd";
 import Admin from "@/components/shared/admin";
 import { useState } from "react";
 import axios from "axios";
+// import AWS from "@/modules/aws.modules";
+
+// const s3 = new AWS.S3();
 
 const { TextArea } = Input;
 
-const items = [
-    {
-      key: '1',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-          1st menu item
-        </a>
-      ),
-    },
-    {
-      key: '4',
-      danger: true,
-      label: 'a danger item',
-    },
-  ];
-
-const addRoute = ()=>{
+const addProduct = ()=>{
     const [messageApi, contextHolder] = message.useMessage();
     const [open, setOpen] = useState(false);
     const [updateData,setupdateData] = useState(null);
+    const [uploading,setUploading] = useState({
+        state:null,
+        progress:0
+    });
     const [form] = Form.useForm();
 
     const showDrawer = () => {
@@ -44,7 +39,47 @@ const addRoute = ()=>{
     const onClose = () => {
         setOpen(false);
     };
-    
+
+    const [isLoading,setisLoading]=useState(false);
+    const [productObejct,setproductObject] = useState([]); // all product stored here
+    const addProductForm = async(e)=>{
+        const formData=e;
+        formData['totalSeat']=10,
+        setisLoading(!isLoading);
+        try{
+            if(updateData!==null){
+                const {data:modifiedData} = await axios.put(`http://localhost:8080/bus/${updateData._id}`,formData);
+                messageApi.success('Product updated');
+                formData['_id']=productObejct[updateData.index]._id;
+                productObejct[updateData.index]=formData
+                onClose();
+                setupdateData(null);
+                console.log(modifiedData);
+            }
+
+            else{
+                const {data} = await axios.post('http://localhost:8080/bus',formData);
+                messageApi.success('Product successfully added');
+                if(data){
+                    return setproductObject((oldData)=>{
+                        return[
+                            ...oldData,
+                            data
+                        ]
+                    });
+                }
+            }
+        }
+        catch(err){
+            messageApi.error("something went wrong try again !");
+        }
+        finally{
+            setisLoading(false);
+            form.resetFields();
+        }
+       
+    }
+
     //add product Button
     const AddproductButton = ()=>{
         setupdateData(null);
@@ -52,11 +87,118 @@ const addRoute = ()=>{
         showDrawer();
     }
 
-    const AddBusForm = (e) => {
-       console.log(e);
+    //image upload
+    const onProductImageUpload = (id, index) =>{
+        const input = document.createElement('INPUT');
+        input.type = 'file';
+        input.click();
+        input.onchange = async () =>{
+            setUploading({
+                ...uploading,
+                state:index
+            });
+            const file =input.files[0];
+            input.remove();
+            const uploader = s3.upload({
+                Bucket : process.env.NEXT_PUBLIC_S3_BUCKET,
+                Key : file.name,
+                Body :file,
+                ACL : 'public-read'
+            });
+            
+            uploader.on('httpUploadProgress',({total,loaded})=>{
+               const percentage =  Math.floor((loaded*100)/total); 
+               setUploading({
+                ...uploading,
+                state:index,
+                progress:percentage
+               });
+            })
+
+            try{
+                const {key} = await uploader.promise();
+                await axios.put(`/api/product/${id}`,{image: key});
+                setUploading({
+                    ...uploading,
+                    state:null,
+                    progress:0
+                   });
+            }
+            catch(err){
+                console.log(err)
+            }
+            
+        }
     }
+
+
+    //product card component
+    const Cards = ({data})=>{
+        {console.log(data)}
+        return(
+            <div className="px-2 flex items-center justify-between bg-[#F4EBFE] hover:bg-yellow-100 col-span-12
+                                transition duration-300 ease-in-out border-t border-zinc-600">
+                                <span className="col-span-2 p-2">
+                                    <h1>{data.busName}</h1>
+                                    <p className="text-zinc-500 text-xs py-1">27 seater 2x2 Ac</p>
+                                </span>
+                                <span className="col-span-2 p-2">
+                                    {data.destinationCity}
+                                    <p className="text-zinc-500 text-xs font-bold">Boarding</p>
+                                </span>
+                                <span className="col-span-2 p-2">
+                                    {data.originCity}
+                                    <p className="text-zinc-500 text-xs font-bold">Dropping</p>
+                                </span>
+                                <span className="col-span-2 p-2">
+                                    {data.totalSeat}
+                                    <p className="text-zinc-500">Left</p>
+                                </span>
+                                <span className="col-span-2 p-2 flex gap-1">
+                                    <p className="font-bold">Seating Rs</p>{data.perSeatPrice}
+                                </span>
+                                <span className="flex gap-2 text-[18px]">
+                                    <i class='bx bx-edit-alt text-blue-500' onClick={()=>updateProduct(data)}/>
+                                    <i class='bx bx-trash-alt text-red-500' onClick={()=>deleteData({id:data._id,index:data.index})}/>
+                                </span>
+                            </div>
+        )
+    }
+
+    const options=[
+        {
+            value: 'Mumbai',
+            label: 'Mumbai',
+        },
+        {
+            value: 'Delhi',
+            label: 'Delhi',
+        },
+        {
+            value: 'Tamil Nadu',
+            label: 'Tamil Nadu',
+        },
+        {
+            value: 'Goa',
+            label: 'Goa',
+        },
+        {
+            value: 'Karnatka',
+            label: 'Karnatka',
+        },
+        {
+            value: 'Chennai',
+            label: 'Chennai',
+        },
+        {
+            value: 'Agra',
+            label: 'Agra',
+        }
+    ]
+
+
     const onChange = (time, timeString) => {
-        form.setFieldsValue({ 'time-of-departure': timeString });
+        form.setFieldsValue({ 'timeOfDeparture': timeString });
       };
 
       const handleChange = (e,option) => {
@@ -64,45 +206,56 @@ const addRoute = ()=>{
         // console.log(`selected ${e}`,option);
       };
 
-      const options=[
-        {
-        value: 'jack',
-        label: 'Jack',
-        },
-        {
-        value: 'lucy',
-        label: 'Lucy',
-        },
-        {
-        value: 'Yiminghe',
-        label: 'yiminghe',
-        },
-        {
-        value: 'disabled',
-        label: 'Disabled',
-        disabled: true,
-        },
-    ]
-
-    //product card component
-    const Cards = ({data})=>{
-        {console.log(data)}
-        return(
-            <h1>Hello Card</h1>
-        )
+    //update
+    const updateProduct =(data) =>{
+        const updateInfo={
+            _id:data._id,
+            index:data.index
+        }
+        setupdateData(updateInfo);
+        form.setFieldsValue(data);
+        showDrawer();
     }
-    
+
+    //deleteData
+    const deleteData = async (productDetail)=>{
+        console.log(productDetail)
+        try{
+            const {data:deletedData} = await axios.delete(`http://localhost:8080/bus/${productDetail.id}`);
+            if(deleteData){
+                const updatedProductObject = productObejct.filter((item, index) => index !== productDetail.index);
+                setproductObject(updatedProductObject);
+                messageApi.success('Product deleted successfully !');
+            }
+        }
+        catch(err){
+            messageApi.warning('something went wrong try again !');
+        }
+    };
+
+    //search Data
+    const searchData =() =>{
+        alert();
+    };
+
+    const [selectedValue, setSelectedValue] = useState();
+    const menu = (
+        <Menu onClick={(e)=>console.log(e.key)}>
+          <Menu.Item key="1">Option 1</Menu.Item>
+          <Menu.Item key="2">Option 2</Menu.Item>
+          <Menu.Item key="3">Option 3</Menu.Item>
+        </Menu>
+      );
     return(
         <Admin>
             {contextHolder}
-            <Drawer title="Add Product" placement="right" closable={true} onClose={onClose} open={true}>
-                
-                <Form form={form} onFinish={AddBusForm}>
-                    <Form.Item name="placeName">
-                        <Input placeholder="Place Name" className="py-2" />
+            <Drawer title="Add Product" placement="right" closable={true} onClose={onClose} open={open}>
+                <Form form={form} onFinish={addProductForm}>
+                    <Form.Item name="busName">
+                        <Input placeholder="Bus Name" className="py-2" />
                     </Form.Item>
                     <div className='flex gap-4'>
-                        <Form.Item name="origin" className="w-full">
+                        <Form.Item name="originCity" className="w-full">
                             <Select
                                 name='origin-city'
                                 className="w-full h-[40px]"
@@ -112,7 +265,7 @@ const addRoute = ()=>{
                             />
                             
                         </Form.Item>
-                        <Form.Item name="destination" className="w-full">
+                        <Form.Item name="destinationCity" className="w-full">
                             <Select
                                 className="w-full h-[40px]"
                                 defaultValue="lucy"
@@ -123,61 +276,68 @@ const addRoute = ()=>{
                     </div>
 
                     <div className='flex gap-4'>
-                        <Form.Item name="total-seat">
+                        <Form.Item name="totalSeatLeft">
                             <Input type='Number' placeholder="Total Sheat" className="py-2" />
                         </Form.Item>
-                        <Form.Item name="per-seat-price">
+                        <Form.Item name="perSeatPrice">
                             <Input type='number' placeholder="Per Seat Price" className="py-2" />
                         </Form.Item>
                     </div>
                     <div className='flex gap-4'>
-                        <Form.Item name="time-of-departure">
+                        <Form.Item name="timeOfDeparture">
                         <Form.Item wrap>
                             <TimePicker className="py-2" use12Hours format="h:mm:ss A" onChange={onChange} />
                         </Form.Item>
                         </Form.Item>
-                        <Form.Item name="date-of-departure">
+                        <Form.Item name="dateofDeparture">
                             <Input type='date' placeholder="Date Of Departure" className="py-2" />
                         </Form.Item>
                     </div>
                     <Button
-                    className={`w-full font-bold h-full py-2 bg-blue-500 cursor-pointer text-white`} 
-                    htmlType='submit' >
-                        Add Bus
+                    className={`w-full font-bold h-full py-2 bg-blue-500 cursor-pointer ${updateData!=null ? 'bg-pink-500':'bg-blue-500'} text-white`} 
+                    htmlType='submit' loading={isLoading}>
+                        {updateData!=null ? 'Update Bus' : 'Add Bus'}
                     </Button>
                     
                 </Form>
             </Drawer>
 
-            <div>
-                    {/*header where search and add product button define*/}
-                    <div className="bg-white flex px-4 justify-between items-center border-b border-zinc-400">
+            {/*header where search and add product button define*/}
+            <div className="flex px-4 justify-between items-center border-b border-zinc-400">
 
-                        <button onClick={AddproductButton} className="bg-blue-500 my-2 text-white p-2 rounded flex items-center gap-1 font-bold">
-                            <i class='bx bx-plus text-xl'/> 
-                            Add Bus
-                        </button>
+                <button onClick={AddproductButton} className="bg-blue-500 my-2 text-white p-2 rounded flex items-center gap-1 font-bold">
+                    <i class='bx bx-plus text-xl'/> 
+                    Add Product
+                </button>
 
-                        <div>
-                            <Form className="flex items-center gap-2" >
-                                
-                                <Dropdown trigger={['click']}>
-                                    <Button>
-                                        'Select an option'
-                                    </Button>
-                                </Dropdown>
-                                <Input type="text" placeholder="Search product" className="py-2"/>
-                                <button className="py-2 px-3 text-white bg-blue-500 rounded font-semibold">Search</button>
-                            </Form>
-                        </div>
-                    </div>
+                <div>
+                    <Form className="flex items-center gap-2" onFinish={searchData}>
+                        
+                        <Dropdown overlay={menu} trigger={['click']}>
+                            <Button>
+                                {selectedValue || 'Select an option'}
+                            </Button>
+                        </Dropdown>
+                        <Input type="text" placeholder="Search product" className="py-2"/>
+                        <button className="py-2 px-3 text-white bg-blue-500 rounded font-semibold">Search</button>
+                    </Form>
+                </div>
+            </div>
 
-                    {/*/check is data added or not*/}
-                    <div className="bg-white mt-2 h-[420px] p-2 overflow-y-scroll">
-                        <h1>lorem1000</h1>
-                    </div>
+            <div className="p-2 grid grid-cols-4 gap-6 items-center">
+                {/*/check is data added or not*/}
+                {!productObejct.length && <Empty/> }
+
+                {/*/display data if added*/}
+                {productObejct && productObejct
+                        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                        .map((items, index) => {
+                        items['index'] = index;
+                            return <Cards data={items} key={index} />;
+                        })}
+
             </div>
         </Admin>
     );
 }
-export default addRoute;
+export default addProduct;
